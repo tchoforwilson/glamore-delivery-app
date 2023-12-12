d02c1234d7b434f9b58c10453c152ff1bb81a4e8
tchoforwilson <tchoforwilson@gmail.com>
2023-12-12 21:47:15 +0100

    Begin implementing home screen

---
+++ src/screens/home/HomeScreen.js
@@ -0,0 +1,11 @@
+import { SearchWidget } from "../../components/searchWidget";
+
+const HomeScreen = () => {
+  return (
+    <main className="home-screen">
+      <SearchWidget />
+    </main>
+  );
+};
+
+export default HomeScreen;
